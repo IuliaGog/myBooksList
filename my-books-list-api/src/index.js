d47dbe1ad26a0fcc52a5acbafb4d56e63c1a058b
Eduard Dinea <edinea@cisco.com>
2019-01-13 20:48:54 +0200

    MondoDB setup

--- my-books-list-api/src/index.js
+++ my-books-list-api/src/index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
+import auth from "./routes/auth";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
+mongoose.connect(
+  "mongodb://localhost/myBooksList",
+  { useMongoClient: true }
+);
 
-app.post("/api/auth", (req, res) => {
-  res.status(400).json({ errors: { global: "Invalid credentials" } });
-});
+app.use("/api/auth", auth);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
