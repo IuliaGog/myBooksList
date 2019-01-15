ad53e1b69b21e3937a978638da10202dab8ff524
Iulia Goganau <iulia.goganau@gmail.com>
2019-01-15 20:48:38 +0200

    API repairs

--- my-books-list-api/src/index.js
+++ my-books-list-api/src/index.js
@@ -1,20 +1,14 @@
 import express from "express";
 import path from "path";
-import mongoose from "mongoose";
-import auth from "./routes/auth";
-import bodyParser from "body-parser";
 
 const app = express();
-app.use(bodyParser.json());
-mongoose.connect(
-  "mongodb://localhost/myBooksList",
-  { useMongoClient: true }
-);
 
-app.use("/api/auth", auth);
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.post("/api/auth", (req, res) => {
+    res.status(400).json({ errors: { global: "Invalid credentials" } });
 });
 
-app.listen(8080, () => console.log("running on local: 8080"));
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(8080, () => console.log("Running on localhost:8080"));
